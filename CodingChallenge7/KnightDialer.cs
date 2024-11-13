fac93075b54d73ca5a1ebca8301109b5f1766a1a
Egemen Çiftci <egemenciftci@gmail.com>
2024-11-13 19:10:03 +0100

    Add project files.

---
+++ CodingChallenge7/KnightDialer.cs
@@ -0,0 +1,59 @@
+﻿using System.Numerics;
+
+namespace CodingChallenge7;
+
+public class KnightDialer
+{
+    private static readonly Dictionary<int, int[]> knightMoves = new()
+    {
+        { 0, [ 4, 6 ] },
+        { 1, [ 6, 8 ] },
+        { 2, [ 7, 9 ] },
+        { 3, [4, 8] },
+        { 4, [0, 3, 9] },
+        { 5, [] },
+        { 6, [0, 1, 7] },
+        { 7, [2, 6] },
+        { 8, [1, 3] },
+        { 9, [2, 4] }
+    };
+
+    private readonly Dictionary<(int, int), BigInteger> memo = [];
+
+    public BigInteger CountDistinctNumbers(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            BigInteger sum = 0;
+            for (int x = 0; x < 10; x++)
+            {
+                sum += CountDistinctFrom(x, n);
+            }
+            return sum;
+        }
+    }
+
+    private BigInteger CountDistinctFrom(int start, int hops)
+    {
+        if (hops == 1)
+        {
+            return 1;
+        }
+
+        if (!memo.TryGetValue((start, hops), out BigInteger sum))
+        {
+            foreach (int x in knightMoves[start])
+            {
+                sum += CountDistinctFrom(x, hops - 1);
+            }
+            memo[(start, hops)] = sum;
+        }
+
+        return sum;
+    }
+}
+
---
+++ CodingChallenge7/Program.cs
@@ -0,0 +1,17 @@
+﻿using CodingChallenge7;
+using System.Numerics;
+
+Console.WriteLine("Started...");
+
+KnightDialer knightDialer = new();
+
+List<(int x, BigInteger)> values = Enumerable.Range(0, 5000)
+    .Select(x => (x, knightDialer.CountDistinctNumbers(x)))
+    .ToList();
+
+var output = string.Join(Environment.NewLine, values.Select(x => $"{x.x:0000} => {x.Item2}"));
+Console.WriteLine(output);
+
+Console.WriteLine("Finished.");
+
+Console.ReadKey();
